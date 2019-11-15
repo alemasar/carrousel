ae9a911f643e7981eb2c35767d800b0fa7984d90
Aleix Masague <alemasar@gmail.com>
2019-11-15 20:53:36 +0100

    Inifinite movement

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -74,18 +74,10 @@ export default {
   },
   mounted() {
     const imagesLi = this.$refs.slider.querySelectorAll("li");
-    // const numImages = imagesLi.length;
-    //let dir = 1;
-
     imagesLi.forEach((img, i) => {
       const left = (img.offsetWidth - this.x_offset) * i;
       const top = this.y_offset * this.top[i];
-      this.initConfig[i] = {};
-      this.initConfig[i].style = {
-        top: top,
-        left: left,
-        "z-index": this.pos[i]
-      };
+      this.initConfig[i] = this.getStyle(left, top, this.pos[i]);
       this.styles.push(this.getStyle(left, top, this.pos[i]));
     });
 
@@ -93,132 +85,44 @@ export default {
   },
   methods: {
     getStyle(left, top, zIndex) {
-      // eslint-disable-next-line no-console
-      console.log(left);
       return {
         transform: `translate(${left}px, ${top}px)`,
         "z-index": zIndex
       };
     },
     animationConfig(element, styleObjElement, delay) {
-      // eslint-disable-next-line no-console
-      console.log(this.getStyle(...styleObjElement));
-      this.styles.splice(element, 1, this.getStyle(...styleObjElement));
-      //this.initConfig.splice(element, 1, this.getStyle(...styleObjElement));
+      this.styles.splice(element, 1, styleObjElement);
+      this.initConfig.splice(element, 1, styleObjElement);
       if (delay === "up") {
         this.isMovingUp.splice(element, 1, true);
       } else {
         this.isMovingDown.splice(element, 1, true);
       }
     },
-    animationMovingUp(index) {
-      /*this.animationConfig(index, [
-        this.initConfig[index].style.left - this.x_offset,
-        this.initConfig[this.pos[2]].style.top,
-        this.pos[2]
-      ]);*/
-      //setTimeout(() => {
-      // eslint-disable-next-line no-console
-      console.log("FIRST ANIMATION FINISHED", index);
-
-      /*this.isMovingUp[index] = false;
-      this.isMovingDown[index] = false;*/
-      //let styles = [];
-
-      this.initConfig.forEach((element, i) => {
-        if (i === this.initConfig.length - 1) {
-          setTimeout(() => {
+    animationMovingUp() {
+      let config = this.initConfig.slice();
+      config.forEach((element, i) => {
+        if (i === config.length - 1) {
+          const animation = () => {
             this.animationConfig(
               i,
-              [
-                this.initConfig[0].style.left,
-                this.initConfig[this.pos[0]].style.top,
-                this.pos[0]
-              ],
+              config[0],
               "down"
             );
-            
-          }, 100 * i);
+          }
+          setTimeout(animation, 100 * i);
 
         } else {
-          setTimeout(() => {
+          const animation = () => {
             this.animationConfig(
               i,
-              [
-                this.initConfig[i + 1].style.left,
-                this.initConfig[this.pos[i + 1]].style.top,
-                this.pos[i + 1]
-              ],
+              config[i + 1],
               "down"
             );
-          }, 100 * i);
+          }
+          setTimeout(animation, 100 * i);
         }
       });
-
-      //setTimeout(() => {
-      /*this.animationConfig(
-        3,
-        [
-          this.initConfig[4].style.left,
-          this.initConfig[this.pos[4]].style.top,
-          this.pos[4]
-        ],
-        "down"
-      );
-      this.animationConfig(
-        4,
-        [
-          this.initConfig[0].style.left,
-          this.initConfig[this.pos[0]].style.top,
-          this.pos[0]
-        ],
-        "down"
-      );
-      this.animationConfig(
-        0,
-        [
-          this.initConfig[1].style.left,
-          this.initConfig[this.pos[1]].style.top,
-          this.pos[1]
-        ],
-        "down"
-      );*/
-      // },2500);
-      // eslint-disable-next-line no-console
-      console.log(this.styles);
-      // setTimeout(() => {
-      // eslint-disable-next-line no-console
-      console.log("thirst ANIMATION INIT");
-      /* styles = [];
-          this.animationState.forEach(ind => {
-            if (ind === index + 1) {
-              styles.push(
-                this.getStyle(
-                  this.initConfig[index + 1].style.left,
-                  this.initConfig[this.pos[index + 1]].style.top,
-                  this.pos[index + 1]
-                )
-              );
-            } else {
-              styles.push(
-                this.getStyle(
-                  this.initConfig[ind].style.left,
-                  this.initConfig[this.pos[ind]].style.top,
-                  this.pos[ind]
-                )
-              );
-            }
-          });
-          this.styles = styles;
-          this.isMovingUp[index + 1] = true;*/
-      /*this.animationConfig(index + 1, [
-          this.initConfig[2].style.left,
-          this.initConfig[this.pos[2]].style.top,
-          this.pos[2]
-        ]);*/
-      // }, 2500);
-      //this.isMoving[index] = true;
-      //}, 5000);
     },
 
     changeImage: function(index) {
@@ -226,20 +130,11 @@ export default {
       this.isMovingDown=[false, false, false, false, false];
       this.isMovingUp=[false, false, false, false, false];
       if (index < this.initConfig.length - 1) {
-        //this.animationState[index] = index + 1;
         this.animationMovingUp(index);
       } else if (index === this.initConfig.length - 1) {
-        //this.animationState[this.isMoving.length - 1] = 0;
         this.animationMovingUp(index);
       } else if (index === this.initConfig.length) {
-        //this.animationState[0] = 1;
       }
-
-      /*    animationEnd:function (){
-        
-        // eslint-disable-next-line no-console
-        console.log("ANIMATION END")
-    }*/
     }
   },
   watch: {
